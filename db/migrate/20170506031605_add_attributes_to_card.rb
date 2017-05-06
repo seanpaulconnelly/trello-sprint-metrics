bb3b4dcb0055d98b2deb100506f08226f0e25581
Sean Connelly <sean@seanconnelly.me>
2017-05-06 10:15:17 -0400

    fixing migration

--- db/migrate/20170506031605_add_attributes_to_card.rb
+++ db/migrate/20170506031605_add_attributes_to_card.rb
@@ -1,6 +1,5 @@
 class AddAttributesToCard < ActiveRecord::Migration[5.1]
   def change
-    add_column :cards, :trello_card_name, :string
     add_reference :cards, :list, foreign_key: true
   end
 end
