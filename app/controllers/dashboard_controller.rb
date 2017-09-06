7f846494007edab26fa62b166b586996ef3170e4
Sean Connelly <sean@seanconnelly.me>
2017-09-06 11:49:05 -0400

    basic dev reporting

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,7 @@
 class DashboardController < ApplicationController
-  before_action :require_login
 
   def show
-    @last_two_weeks_client_stats = DailyKanbanMetric.where(metric_type:1).last(14)
-    @last_two_weeks_bug_stats = DailyKanbanMetric.where(metric_type:2).last(14)
+    @users = User.all
   end
-
-  private
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,19 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
   def show
+    @last_two_weeks_client_work = DailyKanbanMetric.where(user: @user, metric_type: 1).last(14)
+    @last_two_weeks_bug_fixes = DailyKanbanMetric.where(user: @user, metric_type: 2).last(14)
+    @last_two_sprints_velocity = ArchivedMetric.where(user: @user).last(2)
   end
 
   def edit
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        redirect_to @user, notice: 'User was successfully updated.'
-      else
-        render :edit
-      end
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
     end
   end
 
